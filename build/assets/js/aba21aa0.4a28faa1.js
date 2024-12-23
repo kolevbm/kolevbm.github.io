61bebc2d6a87805ac1446cf3e4a01f46ead204c6
kolevbm <kolev.bm@gmail.com>
2024-12-23 20:28:34 +0200

    manual deploy

---
+++ build/assets/js/aba21aa0.4a28faa1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktaking_notes=self.webpackChunktaking_notes||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
