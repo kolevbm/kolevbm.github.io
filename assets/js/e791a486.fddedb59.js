3be4490c0a6583672757577da9191889d5703acd
kolevbm <kolev.bm@gmail.com>
2025-01-01 13:03:23 +0200

    Deploy website - based on 827907826a618e1fde258bc4b943a9bb367c1ddb

---
+++ assets/js/e791a486.fddedb59.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktaking_notes=self.webpackChunktaking_notes||[]).push([[8544],{3692:t=>{t.exports=JSON.parse('{"tag":{"label":"wg","permalink":"/blog/tags/wg","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/wg","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
